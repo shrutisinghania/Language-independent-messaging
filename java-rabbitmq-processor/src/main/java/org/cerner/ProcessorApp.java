4feb480fb5c516ba894531da3a51378afec3c1bf
Rohit Singh <Rohit.Singh@cerner.com>
2020-04-09 12:38:19 +0530

    Adding java processor

---
+++ java-rabbitmq-processor/src/main/java/org/cerner/ProcessorApp.java
@@ -0,0 +1,23 @@
+package org.cerner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.integration.annotation.Transformer;
+
+@EnableBinding(Processor.class)
+@SpringBootApplication
+public class ProcessorApp {
+
+    @Transformer(inputChannel = Processor.INPUT, outputChannel = Processor.OUTPUT)
+    public Object transform(String name) {
+    	System.out.println("Input: " + name);
+    	System.out.println("Output: " + "Hello, " + name + "!");
+    	return "Hello, " + name + "!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessorApp.class, args);
+    }
+}
